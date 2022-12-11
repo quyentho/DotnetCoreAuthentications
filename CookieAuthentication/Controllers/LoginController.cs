748e08ab73285bcd7c127d2eab2b0f7de9666d4b
quyentho <thokimquangquyen@gmail.com>
2022-12-11 15:46:40 +0700

    add custom cookie authentication

---
+++ CookieAuthentication/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CookieAuthentication.Models;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace CookieAuthentication.Controllers;
+
+public class LoginController : Controller
+{
+    private readonly ILogger<LoginController> _logger;
+
+    public LoginController(ILogger<LoginController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> IndexAsync(string username, string password)
+    {
+        if (!ModelState.IsValid) return View();
+
+        if (username == "admin" && password == "admin")
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "user1"),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+            var indentity = new ClaimsIdentity("MyCookieAuth");
+            indentity.AddClaims(claims);
+
+            ClaimsPrincipal principle = new ClaimsPrincipal(indentity);
+            await HttpContext.SignInAsync("MyCookieAuth", principle);
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
